bf91826de15d2e0533c8c590da0f59ccc9a94dfa
rosowskimik <m.rosowski1@wp.pl>
2019-08-30 15:41:54 +0200

    Added mongoose

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(con => console.log('Connected to database'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
