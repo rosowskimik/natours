4530b4bbbb61f96ab38ccfa5eb4a63b438b01f0e
rosowskimik <m.rosowski1@wp.pl>
2019-09-02 17:50:24 +0200

    Updated error controller to send different response in dev vs prod

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,33 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+  } else {
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong'
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
   err.statusCode = err.statusCode || 500;
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
