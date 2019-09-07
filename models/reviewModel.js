b6baaa9b32d7d18fcf3580de6fbea761f2b67df4
rosowskimik <m.rosowski1@wp.pl>
2019-09-07 17:01:18 +0200

    Sending back reviews data in /tours/id GET request

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -35,9 +35,6 @@ const reviewSchema = new mongoose.Schema(
 // Middlewares
 reviewSchema.pre(/^find/, function(next) {
   this.populate({
-    path: 'tour',
-    select: 'name'
-  }).populate({
     path: 'user',
     select: 'name photo'
   });
--- models/tourModel.js
+++ models/tourModel.js
@@ -114,6 +114,12 @@ tourSchema.virtual('durationWeeks').get(function() {
   return (this.duration / 7).toFixed();
 });
 
+tourSchema.virtual('reviews', {
+  ref: 'Review',
+  foreignField: 'tour',
+  localField: '_id'
+});
+
 // Middlewares
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
