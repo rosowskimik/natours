cd10bfec5a39edf94916bba9cb755b31b380c82a
rosowskimik <m.rosowski1@wp.pl>
2019-09-07 13:58:24 +0200

    Updated tourModel
    Added geospacial data fields into model
    Added reference to tour guides from users document

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -20,13 +20,14 @@ mongoose
   .then(con => console.log('Connected to database'));
 
 //Get JSON data
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
-);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 const importData = async () => {
   try {
-    tours.forEach(el => delete el.id);
+    tours.forEach(el => {
+      el.locations.forEach(el2 => delete el2._id);
+      delete el._id;
+    });
     await Tour.create(tours);
     console.log('Data created successfully');
   } catch (error) {
