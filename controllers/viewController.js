3f7100cf19725718bde1b921887255078728c433
rosowskimik <m.rosowski1@wp.pl>
2019-09-18 16:26:37 +0200

    Finished tour details page (w/o the map)

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.getOverview = catchAsync(async (req, res) => {
   const tours = await Tour.find();
@@ -10,8 +11,18 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker'
+exports.getTour = catchAsync(async (req, res, next) => {
+  const { slug } = req.params;
+
+  if (!slug) return next(new AppError('This page does not exist', 404));
+
+  const tour = await Tour.findOne({ slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user'
   });
-};
+
+  res.status(200).render('tour', {
+    title: tour.name,
+    tour
+  });
+});
