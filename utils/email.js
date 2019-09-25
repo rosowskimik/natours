e8ade13eac2d777981cad38526fb0fa92906a0f2
rosowskimik <m.rosowski1@wp.pl>
2019-09-25 20:38:50 +0200

    Updated email utility
    Changed sendEmail function to Email class

--- utils/email.js
+++ utils/email.js
@@ -1,27 +1,63 @@
+const path = require('path');
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async options => {
-  // Create transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD
+class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Natours <${process.env.EMAIL_FROM}>`;
+  }
+
+  newTransporter() {
+    if (process.env.NODE_ENV === 'production') {
+      // Sendgrid
+      return 1;
     }
-  });
 
-  // Set options
-  const mailOptions = {
-    from: 'valid@email.com (Natours)',
-    to: options.email,
-    subject: options.subject,
-    text: options.message
-    // html:
-  };
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    });
+  }
 
-  // Send email
-  await transporter.sendMail(mailOptions);
-};
+  send(template, subject) {
+    const html = pug.renderFile(
+      path.resolve(__dirname, '../views/emails', `${template}.pug`),
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject
+      }
+    );
 
-module.exports = sendEmail;
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html)
+    };
+
+    return this.newTransporter().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send(
+      'welcome',
+      `Welcome ${this.firstName} to the Natours family`
+    );
+  }
+
+  async sendActivate() {
+    await this.send('activate', '<Natours> Account activation');
+  }
+}
+
+module.exports = Email;
