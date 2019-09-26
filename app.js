ece6320b87b5d3891a356ee4cdbcbb5e0b25697d
rosowskimik <m.rosowski1@wp.pl>
2019-09-26 19:28:50 +0200

    Integrated stripe payments into the backend
    Created new bookingRoutes and attached it to /bookings endpoint
    Created new bookingController
    Added stripe as a dependency
    Created new getCheckoutSession handler function for /checkouts

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const hpp = require('hpp');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -70,6 +71,7 @@ app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/bookings', bookingRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server.`, 404));
